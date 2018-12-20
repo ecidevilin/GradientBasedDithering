27c2e5b4ee06399e733b0bdb59a5837cb16a7bf3
Chaos <ecidevilin@live.cn>
2018-12-20 19:53:54 +0800

    utils and math

---
+++ Assets/Utils/ClosestInBits.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chaos
+{
+    public static partial class Utils
+    {
+        public static int ClosestInBits(int val, byte bit)
+        {
+            return (val >> bit << bit);
+        }
+    }
+}
---
+++ Assets/Utils/Swap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chaos
+{
+    public static partial class Utils
+    {
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+}
